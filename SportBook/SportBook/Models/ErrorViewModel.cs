afcd5bc74aad0d44bed101c71e5f83da0710f7a6
Raionn <arturas9631@gmail.com>
2020-03-24 18:03:38 +0200

    Base project

---
+++ SportBook/SportBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SportBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
