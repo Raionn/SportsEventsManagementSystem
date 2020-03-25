5ce4101288bafc092c45e22730d0dc248508eef5
dalzuk <dalius.zukauskas@ktu.edu>
2020-03-25 16:13:57 +0200

    added Login and Registration view and General controller for both of them.. default navigation

---
+++ SportBook/SportBook/Controllers/GeneralController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SportBook.Models;
+
+namespace SportBook.Controllers
+{
+    [Route("[action]")]
+    public class GeneralController : Controller
+    {
+        private readonly ILogger<GeneralController> _logger;
+        public GeneralController(ILogger<GeneralController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult LoginValidation()
+        {
+            //validate login information
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult RegistrationValidation()
+        {
+            //validate login information
+            return RedirectToAction("Login", "General");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
