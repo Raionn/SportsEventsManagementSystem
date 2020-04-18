7194a13636a5055df949ed0fdd3fb8cfb93266cf
Raionn <arturas9631@gmail.com>
2020-04-18 16:44:24 +0300

    Add SQL Server DB

---
+++ SportBook/Sportbook.Database/dbo/Tables/Users.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Users]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Username] NVARCHAR(30) NOT NULL, 
+    [Firstname] NVARCHAR(30) NULL, 
+    [Lastname] NVARCHAR(30) NULL, 
+    [Birthdate] DATE NULL, 
+    [Password] NVARCHAR(200) NOT NULL, 
+    [Email] NVARCHAR(50) NULL
+)
