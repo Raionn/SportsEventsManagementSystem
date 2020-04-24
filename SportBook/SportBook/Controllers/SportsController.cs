235e9ba9ddf1c8dfc409177df142d92a5a3fdc7d
Raionn <arturas9631@gmail.com>
2020-04-24 23:46:03 +0300

    Fix Google Maps, test markers on map

--- SportBook/SportBook/Controllers/SportsController.cs
+++ SportBook/SportBook/Controllers/SportsController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace SportBook.Controllers
 {
 
-    [Authorize]
+    //[Authorize]
     public class SportsController : Controller
     {
         [Route("[action]")]
