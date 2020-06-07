e71c73e1a985b434c324aba32174cce21c695646
dalzuk <dalius.zukauskas@ktu.edu>
2020-06-07 22:46:31 +0300

    Tournament additional information

---
+++ SportBook/SportBook/ViewModels/TeamWithMembers.cs
@@ -0,0 +1,23 @@
+﻿using SportBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportBook.ViewModels
+{
+    public class TeamWithMembers
+    {
+
+        public List<TeamMember> TeamMembers { get; set; }
+        public Team Team { get; set; }
+
+        public TeamWithMembers(List<TeamMember> teamMembers, Team team)
+        {
+            TeamMembers = teamMembers;
+            Team = team;
+        }
+
+
+    }
+}
